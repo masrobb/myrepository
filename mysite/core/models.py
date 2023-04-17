00e14bc2a14a9adcb60c884faf4e1a1b608e61e6
masrobb <61880579+masrobb@users.noreply.github.com>
2023-04-17 11:59:04 +0200

    Delete mysite directory

--- mysite/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Class1(models.Model):
-    field1 = models.CharField(max_length=240)
-    field2 = models.DateField(auto_now=True)
-
